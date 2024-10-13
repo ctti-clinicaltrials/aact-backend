f85641d1eedd911983e62385c2c148e33e037d88
akabishau <aleksey.kabishau@icloud.com>
2024-10-13 19:55:31 -0400

    fix data mapping controller

--- app/controllers/api/v1/data_mapping_controller.rb
+++ app/controllers/api/v1/data_mapping_controller.rb
@@ -2,8 +2,8 @@ module Api
   module V1
     class DataMappingController < ApplicationController
       def index
-        mappings = CtgovApi::Mapping.joins(:ctgov_api_metadata)
-        render json: mappings.to_json(include: :ctgov_api_metadata)
+        mappings = Ctgov::AactMapping.joins(:api_metadata)
+        render json: mappings.to_json(include: :api_metadata)
       end
     end
   end
